0e0ef5cec01f80003cb88505e6902519f53747cc
Barrett Otte <barrettotte@gmail.com>
2023-12-22 12:19:15 -0500

    fixes zsh plugin location

--- setup.sh
+++ setup.sh
@@ -124,8 +124,8 @@ if ! [ -x "$(command -v zsh)" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null
 
   # plugins
-  git clone --quiet https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" > /dev/null
-  git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting "${ZSH_CUSTOM:-HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" > /dev/null
+  git clone --quiet https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" > /dev/null
+  git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" > /dev/null
 fi
 zsh --version
 
